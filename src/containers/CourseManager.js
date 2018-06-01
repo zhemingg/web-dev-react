a923269deb89e2ef9b8045c2b778a6ebb9335663
ZhemingGao <gao19911026@gmail.com>
2018-05-31 17:41:56 -0700

    In CourseManager, added Route to CourseList

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import CourseList from './CourseList';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class CourseManager extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Course Manager</h1>
-                <CourseList/>
-            </div>
+            <Router>
+                <div className="container-fluid">
+                    <h1>Course Manager</h1>
+
+                    <Route path='/courses' component={CourseList}>
+                    </Route>
+                   
+                    <Route path='/examples'>
+                    </Route>
+                </div>
+            </Router>
         )
     }
 }
+
 export default CourseManager;
